27cbbcb8ac1dc6c5bae636e7c37c13c76bd80ea8
Solin Guo <solin.guo@outlook.com>
2017-06-10 19:42:53 +0800

    fixed task_p to task_list

--- rtos/os_queue.c
+++ rtos/os_queue.c
@@ -4,16 +4,17 @@
  */
 
 #include "rtos.h"
+#include "mem.h"
 
 int send_msg_queues(int recv_id, void *msg)
 {
     int count;
-    struct task_p *recv_task;
+    struct task_list *recv_task;
     struct msg_queues_list *queues_list;
 
     recv_task = find_task_by_id(recv_id);
-    count = recv_task->msg_list.msg_count;
-    queues_list = recv_task->msg_list.head;
+    count = recv_task->task->msg_list.msg_count;
+    queues_list = recv_task->task->msg_list.head;
     
     while (count--) {
         queues_list = queues_list->next;
@@ -25,7 +26,7 @@ int send_msg_queues(int recv_id, void *msg)
     queues_list->next->msg.data = msg;
     queues_list = queues_list->next;
 
-    recv_task->status |= 0x0002;
+    recv_task->task->status |= 0x0002;
     return 0;
 }
 
